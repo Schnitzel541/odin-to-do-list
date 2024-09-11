66f7b5fe534b4bbc374953632ebb3f134cf4581b
Schnitzel541 <j.budding122@gmail.com>
2024-09-11 21:46:54 +0200

    Attempt at adding projects

--- src/createProject.js
+++ src/createProject.js
@@ -1,11 +1,46 @@
 const domLogic = (() => {
+    const listItemsArray = [];
+    const newProjectInput = document.createElement("input");
+    const newProjectButton = document.createElement("button");
     const unorderedProjectList = document.querySelector(".project-list");
+
+    let projectsArray = [];
+
+
+    const renderProjects = (arr) => {
+        arr.forEach((item) => {
+            unorderedProjectList.appendChild(item);
+        })
+    }
+    
+    const addProjectToList = (projectName) => {
+        const newProject = document.createElement("li");
+        newProject.className = "project-list-item";
+        newProject.textContent = projectName;
+        newProjectInput.remove();
+        newProjectButton.remove();
+        listItemsArray.pop(newProjectInput);
+        projectsArray.push(newProject);
+        renderProjects(projectsArray);
+    }
+
     const createProjectLogic = () => {
-        const textField = document.createElement("input");
-        textField.type = "text";
-        ! unorderedProjectList.contains(textField) 
-        ? unorderedProjectList.appendChild(textField)
-        : console.error("Can only enter one project at a time");
+        const listItems = document.querySelectorAll(".project-list-item");
+        listItems.forEach((item) => {
+            listItemsArray.push(item);
+        });
+
+        if (!listItemsArray.includes(newProjectInput)) {
+            newProjectInput.value = "New Project";
+            newProjectButton.textContent = "Create Project"
+            unorderedProjectList.appendChild(newProjectInput);
+            unorderedProjectList.appendChild(newProjectButton);
+            listItemsArray.push(newProjectInput);
+            newProjectButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                addProjectToList(newProjectInput.value)
+            })
+        }
     }
     return { createProjectLogic }
 })();
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './styles.css'
 import { createProject } from "./createProject.js";
 import { createTodoObject } from './createTodo.js';
 
-// Project Creation logic
-/****/createProject();
 
 // To-do Creationg Logic
 /****/createTodoObject();
